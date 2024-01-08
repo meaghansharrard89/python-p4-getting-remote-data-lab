916e9e490c7d49e21fdaa421127e038d74f58900
Meaghan <meaghan.sharrard89@gmail.com>
2024-01-08 15:02:42 -0800

    passing tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
